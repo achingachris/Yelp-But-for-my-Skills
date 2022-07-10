bc2b8b3bf3aa874f147a67fa2f12180c8b02cc68
chrisdevcode <achinga.chris@gmail.com>
2022-07-10 15:45:58 +0300

    motion

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,23 +1,21 @@
 import { FaTimes } from 'react-icons/fa'
 import PropTypes from 'prop-types'
-import Card from './shared/Card.jsx'
+import Card from './shared/Card'
 
-const FeedbackItem = ({ item, hanldeDelete }) => {
-  // const handleClick = () => {}
-
+function FeedbackItem({ item, handleDelete }) {
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
-      <button className='close' onClick={() => hanldeDelete(item.id)}>
-        <FaTimes color='red' />
+      <button onClick={() => handleDelete(item.id)} className='close'>
+        <FaTimes color='purple' />
       </button>
       <div className='text-display'>{item.text}</div>
     </Card>
   )
 }
 
-export default FeedbackItem
-
 FeedbackItem.propTypes = {
   item: PropTypes.object.isRequired,
 }
+
+export default FeedbackItem
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,22 +1,50 @@
+import { motion, AnimatePresence } from 'framer-motion'
+import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 
-const FeedbackList = ({ feedback, hanldeDelete }) => {
-
-  // check if there is a feedback
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
-    return <p>No Feedbakc Yet</p>
+    return <p>No Feedback Yet</p>
   }
+
   return (
     <div className='feedback-list'>
-      {feedback.map((item) => (
-        <FeedbackItem
-          key={item.id}
-          item={item}
-          hanldeDelete={hanldeDelete}
-        />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   )
+
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((item) => (
+  //       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // )
+}
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
 }
 
 export default FeedbackList
