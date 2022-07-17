d14e35e098cf0145a4b511cd5a3e4c13a7a141c6
chrisdevcode <achinga.chris@gmail.com>
2022-07-17 18:21:33 +0300

    bring data from file to context

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,16 +1,11 @@
 import { v4 as uuidv4 } from 'uuid'
 import { createContext, useState } from 'react'
+import FeedbackData from '../data/FeedbackData'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'Item from ContextAPI',
-      rating: 5,
-    },
-  ])
+  const [feedback, setFeedback] = useState(FeedbackData)
 
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
