d904efe8a71e4f4defb915ef410ddde70b4b7144
itschrisagain <chris.achinga@student.moringaschool.com>
2022-10-18 17:17:26 +0300

    header

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <FeedbackProvider>
       <Router>
-        <Header />
+        <Header text='Yelp Me' />
         <div className='container'>
           <Routes>
             <Route path='/' element={<HomePage />} />
